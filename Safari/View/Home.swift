83d22ad813dd1eb95bc22058a0da70ca5686a86d
Sharkzuria <baumabulondo@gmail.com>
2022-12-04 23:02:29 +0300

    feat: Add  and Design FlightDetailView

--- Safari/View/Home.swift
+++ Safari/View/Home.swift
@@ -28,13 +28,39 @@ struct Home: View {
 //                    .frame(width: size.width * 0.4)
                     .frame(height: 50)
                     .frame(maxWidth: .infinity, alignment: .leading)
+            
+            HStack{
+               FlightDetailView(place: "Nairobi", code: "NBO", timing: "04 Dec, 23:15")
+                
+                VStack(spacing: 8){
+                    Image(systemName: "chevron.right")
+                        .font(.title2)
+                    
+                    Text("8h 28m")
+                }
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity)
+                FlightDetailView(alignment: .trailing, place: "Paris", code: "CDG", timing: "05 Dec, 05:50 + 1")
+            }
+            .padding(.top, 20)
+
+            // MARK: - Airplane Image View
+            Image("Airplane")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(height: 150)
+                .padding(.bottom, -20)
+            
         }
         .padding(15)
         .padding(.top, safeArea.top)
         .background{
             Rectangle()
                 .fill(.linearGradient(colors:[
-                    Color("ElectronBlue"),
+                    Color("PicoVoid"),
+//                    Color("MazarineBlue"),
+                    Color("BrightGreek"),
                     Color("ElectronBlue"),
                     Color("DarnerTailBlue")
                 ], startPoint: .top, endPoint: .bottom))
@@ -42,8 +68,21 @@ struct Home: View {
         }
     }
     @ViewBuilder
-    func FlightDetailView(alignment: HorizontalAlignment= .leading, place:String, timing: String) -> some View{
+    func FlightDetailView(alignment: HorizontalAlignment = .leading, place: String, code: String, timing: String) -> some View{
         
+        VStack(alignment: alignment, spacing: 6) {
+            Text(place)
+                .font(.caption)
+                .foregroundColor(.white.opacity(0.8))
+            
+            Text(code)
+                .font(.title)
+                .foregroundColor(.white)
+            Text(timing)
+                .font(.caption)
+                .foregroundColor(.white.opacity(0.8))
+        }
+        .frame(maxWidth: .infinity)
         
     }
 }
