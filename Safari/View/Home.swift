d5d42fcd416843f1ff9ad9dcaeac3eea1e8e7e40
Sharkzuria <baumabulondo@gmail.com>
2022-12-04 22:19:59 +0300

    style: Build Home View

--- Safari/View/Home.swift
+++ Safari/View/Home.swift
@@ -8,13 +8,50 @@
 import SwiftUI
 
 struct Home: View {
+    
+    // MARK: - View Bounds
+    var size: CGSize
+    var safeArea: EdgeInsets
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0){
+            HeaderView()
+        }
+    }
+    
+    
+    @ViewBuilder
+    func HeaderView() -> some View {
+        VStack{
+                Image("logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+//                    .frame(width: size.width * 0.4)
+                    .frame(height: 50)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .padding(15)
+        .padding(.top, safeArea.top)
+        .background{
+            Rectangle()
+                .fill(.linearGradient(colors:[
+                    Color("ElectronBlue"),
+                    Color("ElectronBlue"),
+                    Color("DarnerTailBlue")
+                ], startPoint: .top, endPoint: .bottom))
+                
+        }
+    }
+    @ViewBuilder
+    func FlightDetailView(alignment: HorizontalAlignment= .leading, place:String, timing: String) -> some View{
+        
+        
     }
 }
 
+
+
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
-        Home()
+        ContentView()
     }
 }
