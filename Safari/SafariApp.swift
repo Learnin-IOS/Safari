a0e62fe6871db020a4ff71866834d43a2d8b9da7
Sharkzuria <baumabulondo@gmail.com>
2022-12-04 17:54:25 +0300

    Initial Commit

---
+++ Safari/SafariApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafariApp.swift
+//  Safari
+//
+//  Created by Le Bon B' Bauma on 04/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct SafariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
