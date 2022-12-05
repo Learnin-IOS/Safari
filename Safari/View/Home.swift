3b2b3b1f5ae22bff0013eeddc8b691689fd89856
Sharkzuria <baumabulondo@gmail.com>
2022-12-05 05:48:12 +0300

    feat: Add Detail View for Flight Intinary

--- Safari/View/Home.swift
+++ Safari/View/Home.swift
@@ -75,24 +75,6 @@ struct Home: View {
                 
         }
     }
-    @ViewBuilder
-    func FlightDetailView(alignment: HorizontalAlignment = .leading, place: String, code: String, timing: String) -> some View{
-        
-        VStack(alignment: alignment, spacing: 6) {
-            Text(place)
-                .font(.caption)
-                .foregroundColor(.white.opacity(0.8))
-            
-            Text(code)
-                .font(.title)
-                .foregroundColor(.white)
-            Text(timing)
-                .font(.caption)
-                .foregroundColor(.white.opacity(0.8))
-        }
-        .frame(maxWidth: .infinity)
-        
-    }
     
     
     // MARK: - Payments
@@ -213,24 +195,109 @@ struct Home: View {
         }
         
     }
+
 }
 
 
 
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView()
+        ContentView()
+    }
+}
+
+// MARK: - Detail View UI
+
+struct FlightDetailView: View {
+    var alignment: HorizontalAlignment = .leading
+    var place: String
+    var code: String
+    var timing:  String
+    
+    
+    var body: some View{
+        VStack(alignment: alignment, spacing: 6) {
+            Text(place)
+                .font(.caption)
+                .foregroundColor(.white.opacity(0.8))
+            
+            Text(code)
+                .font(.title)
+                .foregroundColor(.white)
+            Text(timing)
+                .font(.caption)
+                .foregroundColor(.white.opacity(0.8))
+        }
+        .frame(maxWidth: .infinity)
     }
 }
 
 
-
-// MARK: - Detail View UI
-
 struct DetailView: View {
+    
+    var size: CGSize
+    var safeArea: EdgeInsets
     var body: some View{
-        VStack(spacong: 0){
+        VStack {
+            VStack(spacing: 0) {
+                VStack{
+                    
+                    Image("logo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100)
+                    
+                    
+                    Text("Your order has been submitted")
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .padding(.top, 10)
+                    
+                    
+                    Text("We arewaiting for your booking confirmation")
+                        .font(.caption)
+                        .foregroundColor(.white.opacity(0.8))
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.top, 30)
+                .padding(.bottom, 40)
+                .background(
+                    RoundedRectangle(cornerRadius: 18, style: .continuous)
+                        .fill(.white.opacity(0.1)))
+                HStack{
+                   FlightDetailView(place: "Nairobi", code: "NBO", timing: "04 Dec, 23:15")
+                    
+                    VStack(spacing: 8){
+                        Image(systemName: "chevron.right")
+                            .font(.title2)
+                        
+                        Text("8h 28m")
+                    }
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity)
+                    FlightDetailView(alignment: .trailing, place: "Paris", code: "CDG", timing: "05 Dec, 05:50 + 1")
+                }
+                .padding(15)
+                .padding(.bottom, 70)
+                .background {
+                    RoundedRectangle(cornerRadius: 15, style: .continuous)
+                        .fill(.ultraThinMaterial)
+                }
+                .padding(.top, -20)
+            }
             
+            // 
         }
+        .padding(.horizontal, 20)
+        .padding(.top, safeArea.top + 15)
+        .padding([.horizontal, .bottom], 15)
+        .background {
+            Rectangle()
+                .fill(Color("PicoVoid"))
+                .padding(.bottom, 80)
+        }
+        
     }
+    
 }
