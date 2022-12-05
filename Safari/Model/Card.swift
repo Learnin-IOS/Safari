ab9c27f94d10f4d797dec03838913531de7d1a8e
Sharkzuria <baumabulondo@gmail.com>
2022-12-05 03:08:43 +0300

    feat: Add Card Model

--- Safari/Model/Card.swift
+++ Safari/Model/Card.swift
@@ -7,3 +7,28 @@
 
 import SwiftUI
 
+
+// MARK: - Card Model With Smaple Cards
+
+
+struct Card: Identifiable{
+    var id: UUID = .init()
+    var cardImage: String
+}
+
+
+var sampleCard: [Card] = [
+    .init(cardImage: "card1"),
+    .init(cardImage: "card2"),
+    .init(cardImage: "card3"),
+    .init(cardImage: "card4"),
+    .init(cardImage: "card5"),
+    .init(cardImage: "card6"),
+    .init(cardImage: "card7"),
+    .init(cardImage: "card8"),
+    .init(cardImage: "card9"),
+    .init(cardImage: "card10"),
+
+]
+    
+
